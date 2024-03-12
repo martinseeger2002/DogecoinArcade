28ecd0f7583faf160012fc2b05597e05e1d33d13
₿ig Chief Energy <57598081+martinseeger2002@users.noreply.github.com>
2024-03-11 21:03:52 -0500

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import dogecoin_transaction_details as dt
 NODE_RPC_HOST = "127.0.0.1:22555"
 NODE_RPC_USER = "your_rpc_user"
 NODE_RPC_PASS = "your_rpc_pass"
-TXID = "f32ddc0bf29d46ef2460dc8e7dc071790237195160e294a462cdeb698f146f3d"
+TXID = "5449b51c939356353edbede8e55989a6c1cd4479d8fbe4a8a7da5cec04e1fc59"
 
 scriptSig_list = []  # Initialize the list to store 'scriptSig' data
 
